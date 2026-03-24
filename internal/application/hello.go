0b5feb12918f94f72fc279835287bc6bb0e06c1e
Tomida Takuya <tomida-t@genki.co.jp>
2026-03-24 04:01:13 +0000

    コメント修正

--- internal/application/hello.go
+++ internal/application/hello.go
@@ -1,8 +1,6 @@
 // Package application はサービスのアプリケーション層のユースケースを実装します。
 package application
 
-import "errors"
-
 type helloUsecase struct{}
 
 // NewHelloUsecase は HelloUsecase の実装を生成します。
@@ -10,8 +8,7 @@ func NewHelloUsecase() HelloUsecase {
 	return &helloUsecase{}
 }
 
-// GetHello はあいさつメッセージを返します（現在はシミュレーション上のエラーを返します）。
+// GetHello はあいさつメッセージを返します。
 func (u *helloUsecase) GetHello() (string, error) {
-	// Simulate an error for demonstration
-	return "", errors.New("simulated error")
+	return "Hello, world!", nil
 }
